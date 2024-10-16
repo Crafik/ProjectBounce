dcf10bb29ea217cd38619d69bb70cd4df2941edf
Crafik <gilyazov.007@yandex.ru>
2024-10-16 20:42:08 +0800

    fixed finishing dissapearance anim

--- Assets/AgentAssets/BouncerAssets/Scripts/BouncerFinish.cs
+++ Assets/AgentAssets/BouncerAssets/Scripts/BouncerFinish.cs
@@ -7,7 +7,6 @@ public class BouncerFinish : MonoBehaviour
 {
     public static event Action LevelFinished;
     
-    [SerializeField] private Rigidbody _rigidBody;
     [SerializeField] private GameObject _ball;
 
     private bool isFinishing;
@@ -26,11 +25,11 @@ public class BouncerFinish : MonoBehaviour
 
     private IEnumerator FinishingCoroutine(Vector3 finPos){
         float counter = 0f;
-        Vector3 startPos = _rigidBody.position;
+        Vector3 startPos = transform.position;
         Vector3 startScale = _ball.transform.localScale;
         while (counter < 1f){
             counter += Time.deltaTime;
-            _rigidBody.MovePosition(Vector3.Lerp(startPos, finPos, counter));
+            transform.position = Vector3.Lerp(startPos, finPos, counter);
             _ball.transform.localScale = Vector3.Lerp(startScale, Vector3.zero, counter);
             yield return null;
         }
