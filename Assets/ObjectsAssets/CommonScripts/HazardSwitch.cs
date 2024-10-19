0d3b3fb9cd0830eec014b13a421ff0eef69bf65d
Crafik <gilyazov.007@yandex.ru>
2024-10-19 17:06:05 +0800

    added disables starting state for hazards, added level 5

--- Assets/ObjectsAssets/CommonScripts/HazardSwitch.cs
+++ Assets/ObjectsAssets/CommonScripts/HazardSwitch.cs
@@ -6,6 +6,15 @@ public class HazardSwitch : MonoBehaviour, IActivatable
 {
     [SerializeField] private GameObject _hazard;
 
+    [Space (15)]
+    public bool isOff;
+
+    void Start(){
+        if (isOff){
+            _hazard.SetActive(false);
+        }
+    }
+
     public void Activate(){
         // here be code
         _hazard.SetActive(!_hazard.activeSelf);
